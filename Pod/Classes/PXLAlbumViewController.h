72bf5840d3b24a1816aa8e67f3e75b359380254b
Tim Shi <timgshi@gmail.com>
2015-04-30 15:02:55 -0700

    setting up what will contain the collection view

---
+++ Pod/Classes/PXLAlbumViewController.h
@@ -0,0 +1,13 @@
+//
+//  PXLAlbumViewController.h
+//  Pods
+//
+//  Created by Tim Shi on 4/30/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PXLAlbumViewController : UIViewController
+
+@end
