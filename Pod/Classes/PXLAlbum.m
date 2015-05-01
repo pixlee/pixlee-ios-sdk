5eec143dcc43d976afa6958c4dcff83db2811c65
Tim Shi <timgshi@gmail.com>
2015-05-01 16:21:16 -0700

    adding comments to note how the photo loading task works

--- Pod/Classes/PXLAlbum.m
+++ Pod/Classes/PXLAlbum.m
@@ -59,8 +59,10 @@ const NSInteger PXLAlbumDefaultPerPage = 30;
 
 - (NSURLSessionDataTask *)loadNextPageOfPhotos:(void (^)(NSArray *photos, NSError *error))completionBlock {
     static NSString * const PXLAlbumGETRequestString = @"albums/%@/photos";
+    // make sure there's more content to load
     if (self.hasNextPage) {
         NSInteger nextPage = self.lastPageFetched == NSNotFound ? 1 : self.lastPageFetched + 1;
+        // make sure we aren't already loading that page
         if (!self.loadingOperations[@(nextPage)]) {
             NSString *requestString = [NSString stringWithFormat:PXLAlbumGETRequestString, self.identifier];
             NSMutableDictionary *params = @{}.mutableCopy;
@@ -84,6 +86,7 @@ const NSInteger PXLAlbumDefaultPerPage = 30;
                 self.hasNextPage = [responseObject[@"next"] boolValue];
                 if (photos) {
                     NSMutableArray *mutablePhotos = self.photos.mutableCopy;
+                    // add filler photos if necessary (in the case that page 2 loads before page 1)
                     if (mutablePhotos.count != self.perPage * (nextPage - 1)) {
                         NSInteger numPhotosToAdd = (self.perPage * (nextPage - 1)) - mutablePhotos.count;
                         for (int i = 0; i < numPhotosToAdd; i++) {
@@ -93,6 +96,8 @@ const NSInteger PXLAlbumDefaultPerPage = 30;
                     [mutablePhotos addObjectsFromArray:photos];
                     self.photos = mutablePhotos;
                 }
+                // release the data task by replacing it with a BOOL
+                self.loadingOperations[@(nextPage)] = @YES;
                 completionBlock(photos, nil);
             } failure:^(NSURLSessionDataTask * __unused task, NSError *error) {
                 if (completionBlock) {
