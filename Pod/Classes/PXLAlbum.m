87aff48704130db9893c53166bc57ab1feaf5b4f
Tim Shi <timgshi@gmail.com>
2015-05-01 16:18:42 -0700

    got paging working pretty reliably

--- Pod/Classes/PXLAlbum.m
+++ Pod/Classes/PXLAlbum.m
@@ -16,10 +16,19 @@
 @property (nonatomic, strong) NSArray *photos;
 @property (nonatomic) NSInteger lastPageFetched;
 @property (nonatomic) BOOL hasNextPage;
+@property (nonatomic, strong) NSMutableDictionary *loadingOperations;
 
 @end
 
 @implementation PXLAlbum
+
+// HOW TO DEAL WITH PAGING
+// 1. keep a dictionary of the loading operations keyed by page num
+// 2. when a loading op completes, fill in index gaps with blank PXLPhoto objects if necessary
+// 3. add photos to their correct indices
+// 4. keep track of loaded pages to prevent reloads
+
+const NSInteger PXLAlbumDefaultPerPage = 30;
 
 + (instancetype)albumWithIdentifier:(NSString *)identifier {
     PXLAlbum *album = [self new];
@@ -29,10 +38,11 @@
 
 - (instancetype)init {
     self = [super init];
-    self.perPage = NSNotFound;
+    self.perPage = PXLAlbumDefaultPerPage;
     self.lastPageFetched = NSNotFound;
     self.hasNextPage = YES;
     self.photos = @[];
+    self.loadingOperations = @{}.mutableCopy;
     return self;
 }
 
@@ -50,39 +60,57 @@
     self.photos = @[];
     self.lastPageFetched = NSNotFound;
     self.hasNextPage = YES;
+    [self.loadingOperations removeAllObjects];
 }
 
 - (NSURLSessionDataTask *)loadNextPageOfPhotos:(void (^)(NSArray *photos, NSError *error))completionBlock {
     static NSString * const PXLAlbumGETRequestString = @"albums/%@/photos";
     if (self.hasNextPage) {
-        NSString *requestString = [NSString stringWithFormat:PXLAlbumGETRequestString, self.identifier];
-        NSMutableDictionary *params = @{}.mutableCopy;
-        if (self.sortOptions) {
-            params[@"sort"] = [self.sortOptions urlParamString];
-        }
-        if (self.filterOptions) {
-            params[@"filter"] = [self.filterOptions urlParamString];
-        }
-        if (self.lastPageFetched != NSNotFound) {
-            params[@"page"] = @(self.lastPageFetched + 1);
-        }
-        return [[PXLClient sharedClient] GET:requestString parameters:params success:^(NSURLSessionDataTask * __unused task, id responseObject) {
-            NSArray *responsePhotos = responseObject[@"data"];
-            NSArray *photos = [PXLPhoto photosFromArray:responsePhotos inAlbum:self];
-            self.lastPageFetched = [responseObject[@"page"] integerValue];
-            self.hasNextPage = [responseObject[@"next"] boolValue];
-            if (photos) {
-                NSMutableArray *mutablePhotos = self.photos.mutableCopy;
-                [mutablePhotos addObjectsFromArray:photos];
-                [mutablePhotos insertObjects:photos atIndexes:nil];
-                self.photos = mutablePhotos;
+        NSInteger nextPage = self.lastPageFetched == NSNotFound ? 1 : self.lastPageFetched + 1;
+        if (!self.loadingOperations[@(nextPage)]) {
+            NSString *requestString = [NSString stringWithFormat:PXLAlbumGETRequestString, self.identifier];
+            NSMutableDictionary *params = @{}.mutableCopy;
+            if (self.sortOptions) {
+                params[@"sort"] = [self.sortOptions urlParamString];
             }
-            completionBlock(photos, nil);
-        } failure:^(NSURLSessionDataTask * __unused task, NSError *error) {
-            if (completionBlock) {
-                completionBlock(nil, error);
+            if (self.filterOptions) {
+                params[@"filter"] = [self.filterOptions urlParamString];
             }
-        }];
+            if (self.lastPageFetched != NSNotFound) {
+                params[@"page"] = @(self.lastPageFetched + 1);
+            }
+            NSURLSessionDataTask *dataTask = [[PXLClient sharedClient] GET:requestString parameters:params success:^(NSURLSessionDataTask * __unused task, id responseObject) {
+                NSArray *responsePhotos = responseObject[@"data"];
+                NSArray *photos = [PXLPhoto photosFromArray:responsePhotos inAlbum:self];
+                if (self.lastPageFetched == NSNotFound) {
+                    self.lastPageFetched = [responseObject[@"page"] integerValue];
+                } else {
+                    self.lastPageFetched = MAX(self.lastPageFetched, [responseObject[@"page"] integerValue]);
+                }
+                self.hasNextPage = [responseObject[@"next"] boolValue];
+                if (photos) {
+                    NSMutableArray *mutablePhotos = self.photos.mutableCopy;
+                    if (mutablePhotos.count != self.perPage * (nextPage - 1)) {
+                        NSInteger numPhotosToAdd = (self.perPage * (nextPage - 1)) - mutablePhotos.count;
+                        for (int i = 0; i < numPhotosToAdd; i++) {
+                            [mutablePhotos addObject:[PXLPhoto new]];
+                        }
+                    }
+                    [mutablePhotos addObjectsFromArray:photos];
+                    self.photos = mutablePhotos;
+                }
+                completionBlock(photos, nil);
+            } failure:^(NSURLSessionDataTask * __unused task, NSError *error) {
+                if (completionBlock) {
+                    completionBlock(nil, error);
+                }
+            }];
+            self.loadingOperations[@(nextPage)] = dataTask;
+            return dataTask;
+        } else {
+            completionBlock(nil, nil);
+            return nil;
+        }
     } else {
         completionBlock(nil, nil);
         return nil;
