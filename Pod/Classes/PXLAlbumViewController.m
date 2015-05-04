dc7fdbda39ae08c7ddd5c500a16b36b22d23efae
Tim Shi <timgshi@gmail.com>
2015-05-04 16:55:09 -0700

    using icon images for the grid list toggle

--- Pod/Classes/PXLAlbumViewController.m
+++ Pod/Classes/PXLAlbumViewController.m
@@ -67,9 +67,9 @@ const CGFloat PXLAlbumViewControllerDefaultMargin = 15;
     const CGFloat kButtonTopMargin = 20;
     const CGFloat kButtonHeight = 44;
     
-    self.gridButton = [self displayButtonWithTitle:NSLocalizedString(@"Grid", nil) selector:@selector(displayButtonPressed:)];
+    self.gridButton = [self displayButtonWithImageName:@"pixlee-ios-sdk.bundle/grid" selector:@selector(displayButtonPressed:)];
     [self.view addSubview:self.gridButton];
-    self.listButton = [self displayButtonWithTitle:NSLocalizedString(@"List", nil) selector:@selector(displayButtonPressed:)];
+    self.listButton = [self displayButtonWithImageName:@"pixlee-ios-sdk.bundle/column" selector:@selector(displayButtonPressed:)];
     [self.view addSubview:self.listButton];
     
     self.gridButton.enabled = self.albumDisplayMode != PXLAlbumViewControllerDisplayModeGrid;
@@ -89,10 +89,10 @@ const CGFloat PXLAlbumViewControllerDefaultMargin = 15;
     }];
 }
 
-- (UIButton *)displayButtonWithTitle:(NSString *)title selector:(SEL)selector {
+- (UIButton *)displayButtonWithImageName:(NSString *)imageName selector:(SEL)selector {
     UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
     button.backgroundColor = [UIColor whiteColor];
-    [button setTitle:title forState:UIControlStateNormal];
+    [button setImage:[UIImage imageNamed:imageName] forState:UIControlStateNormal];
     [button setTitleColor:self.view.tintColor forState:UIControlStateNormal];
     [button setTitleColor:[UIColor grayColor] forState:UIControlStateDisabled];
     [button addTarget:self action:selector forControlEvents:UIControlEventTouchUpInside];
