b13ab5efdfd65c53f4deb41151045d19276ee130
csacsika <csaba@bitraptors.com>
2020-03-18 13:23:28 +0100

    added analyitcs view controller

---
+++ Classes/AnalyticsEvents/PXLAnalyitcsEventWidgetVisible.swift
@@ -0,0 +1,45 @@
+//
+//  PXLAnalyitcsEventWidgetVisible.swift
+//  PixleeSDK
+//
+//  Created by Csaba Toth on 2020. 03. 18..
+//  Copyright © 2020. Pixlee. All rights reserved.
+//
+
+
+import UIKit
+
+public struct PXLAnalyticsEventWidgetVisible: PXLAnalyticsEvent {
+    let album: PXLAlbum
+
+    let widget: PXLWidgetType
+
+    public var eventName = "widgetVisible"
+
+    public init(album: PXLAlbum, widget: PXLWidgetType) {
+        self.album = album
+        self.widget = widget
+    }
+
+    public var logParameters: [String: Any] {
+        let udid = UIDevice.current.identifierForVendor?.uuidString ?? "Unknown udid"
+
+        var parameters: [String: Any] = ["widget": widget.key,
+                                         "platform": "ios",
+                                         "uid": udid]
+        if let identifier = album.identifier {
+            parameters["album_id"] = identifier
+        } else {
+            parameters["album_id"] = ""
+            print("PIXLEE SDK:⚠️ Warning you are sending the event without having an album_id. Please wait for the loadMore to return before triggering this event. refer to Readme ")
+        }
+
+        let photoIds = album.photos.compactMap({ (photo) -> String? in
+            String(photo.albumPhotoId)
+        })
+
+        parameters["photos"] = photoIds.joined(separator: ",")
+
+        return parameters
+    }
+}
---
+++ Example/Example/ViewControllers/AnalyticsViewController.swift
@@ -0,0 +1,128 @@
+//
+//  AnalyticsViewController.swift
+//  Example
+//
+//  Created by Csaba Toth on 2020. 03. 18..
+//  Copyright © 2020. Pixlee. All rights reserved.
+//
+
+import PixleeSDK
+import UIKit
+
+class AnalyticsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        var photoAlbumId = ProcessInfo.processInfo.environment["PIXLEE_PHOTO_ALBUM_ID"]
+        photoAlbumId = "402593466"
+        if let photoAlbumId = photoAlbumId {
+            _ = PXLClient.sharedClient.getPhotoWithPhotoAlbumId(photoAlbumId: photoAlbumId) { newPhoto, error in
+                guard error == nil else {
+                    print("Error during load of image with Id \(String(describing: error))")
+                    return
+                }
+                guard let photo = newPhoto else {
+                    print("cannot find photo")
+                    return
+                }
+                self.photo = photo
+            }
+        }
+    }
+
+    var photo: PXLPhoto?
+
+    @IBOutlet var consoleLabel: UILabel!
+
+    // Album events
+    let album = PXLAlbum(identifier: "5984962")
+    @IBAction func openedWidget(_ sender: Any) {
+        _ = PXLAnalyticsService.sharedAnalytics.logEvent(event: PXLAnalyticsEventOpenedWidget(album: album, widget: .other(customValue: "customWidgetName"))) { error in
+            guard error == nil else {
+                self.printToConsole(log: "🛑 There was an error \(error?.localizedDescription ?? "")")
+                return
+            }
+            print("Example opened analytics logged")
+            self.printToConsole(log: "Opened widget fired")
+        }
+    }
+
+    @IBAction func widgetVisible(_ sender: Any) {
+        _ = PXLAnalyticsService.sharedAnalytics.logEvent(event: PXLAnalyticsEventWidgetVisible(album: album, widget: .other(customValue: "customWidgetName"))) { error in
+            guard error == nil else {
+                self.printToConsole(log: "🛑 There was an error \(error?.localizedDescription ?? "")")
+                return
+            }
+            print("Example widget visible analytics logged")
+            self.printToConsole(log: "Widget visible fired")
+        }
+    }
+
+    @IBAction func widgetsExample(_ sender: Any) {
+        let exampleVC = WidgetsExampleViewController(nibName: "WidgetsExampleViewController", bundle: Bundle.main)
+        present(exampleVC, animated: true, completion: nil)
+    }
+
+    @IBAction func loadMore(_ sender: Any) {
+        _ = album.triggerEventLoadMoreTapped { error in
+            guard error == nil else {
+                self.printToConsole(log: "🛑 There was an error \(error?.localizedDescription ?? "")")
+                return
+            }
+            self.printToConsole(log: "Load more fired")
+        }
+    }
+
+    @IBAction func openedLightbox(_ sender: Any) {
+        if let photo = photo {
+            _ = photo.triggerEventOpenedLightbox { error in
+                guard error == nil else {
+                    self.printToConsole(log: "🛑 There was an error \(error?.localizedDescription ?? "")")
+                    return
+                }
+                self.printToConsole(log: "Opened lightbox fired")
+            }
+        }
+    }
+
+    @IBAction func actionClicked(_ sender: Any) {
+        if let photo = photo {
+            _ = photo.triggerEventActionClicked(actionLink: "actionLink", completionHandler: { error in
+                guard error == nil else {
+                    self.printToConsole(log: "🛑 There was an error \(error?.localizedDescription ?? "")")
+                    return
+                }
+                self.printToConsole(log: "Action clicked fired")
+            })
+        }
+    }
+
+    @IBAction func addToCart(_ sender: Any) {
+        _ = PXLAnalyticsService.sharedAnalytics.logEvent(event: PXLAnalyticsEventAddCart(sku: "SL-BENJ", quantity: 1, price: "13.0", currency: "USD")) { error in
+            guard error == nil else {
+                self.printToConsole(log: "🛑 There was an error \(error?.localizedDescription ?? "")")
+                return
+            }
+            self.printToConsole(log: "Add to Cart fired")
+        }
+    }
+
+    @IBAction func conversion(_ sender: Any) {
+        _ = PXLAnalyticsService.sharedAnalytics.logEvent(event:
+            PXLAnalyticsEventConvertedPhoto(cartContents: [PXLAnalyticsCartContents(price: "13.0", productSKU: "SL-BENJ", quantity: 1),
+                                                           PXLAnalyticsCartContents(price: "5.0", productSKU: "AD-1324S", quantity: 2)],
+                                            cartTotal: "18.0",
+                                            cartTotalQuantity: 3,
+                                            currency: "USD")) { error in
+            guard error == nil else {
+                self.printToConsole(log: "🛑 There was an error \(error?.localizedDescription ?? "")")
+                return
+            }
+            self.printToConsole(log: "Conversion fired")
+        }
+    }
+
+    func printToConsole(log: String) {
+        consoleLabel.text = log
+    }
+}
