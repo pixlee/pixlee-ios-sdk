b7147c75ba65c46933b72381707193ee1564526c
csacsika <csaba@bitraptors.com>
2020-08-25 10:43:43 +0200

    added currency and price to product

--- Classes/PXLProduct.swift
+++ Classes/PXLProduct.swift
@@ -18,4 +18,11 @@ public struct PXLProduct {
     public let title: String?
     public let sku: String?
     public let productDescription: String?
+    public let price: Double?
+    public let currency: String?
+
+    var formattedPrice: String? {
+        guard let currency = currency, let price = price else { return nil }
+        return "\(currency) \(price)"
+    }
 }
