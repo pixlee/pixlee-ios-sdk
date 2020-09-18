6beef4634b31b5e97a2e2f8a708ab297c895a502
csacsika <csaba@bitraptors.com>
2020-09-18 15:43:06 +0200

    commented out not needed things

--- Classes/Widgets/PXLPhotoListView.swift
+++ Classes/Widgets/PXLPhotoListView.swift
@@ -7,31 +7,31 @@
 
 import UIKit
 
-class PXLPhotoListView: UIView {
-    let tableView = UITableView()
-
-    var items: [PXLPhoto] = []
-
-    init() {
-        tableView.dataSource = self
-        addSubview(tableView)
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
-
-extension PXLPhotoListView: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        1
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1000
-    }
-}
+//class PXLPhotoListView: UIView {
+//    let tableView = UITableView()
+//
+//    var items: [PXLPhoto] = []
+//
+//    init() {
+//        tableView.dataSource = self
+//        addSubview(tableView)
+//    }
+//
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//}
+//
+//extension PXLPhotoListView: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        
+//    }
+//    
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        1
+//    }
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 1000
+//    }
+//}
