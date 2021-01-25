fd6bae0e0fd31480935b29ac3bae6f589c116ff5
Sungjun <sungjun.app@gmail.com>
2021-01-25 17:57:49 +0900

    Remove unused tests

--- Example/ExampleUITests/ExampleUITests.swift
+++ Example/ExampleUITests/ExampleUITests.swift
@@ -19,35 +19,13 @@ class ExampleUITests: XCTestCase {
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
     
     func testAnalytics() {
-        
         let app = XCUIApplication()
+        app.launch()
+        
         let elementsQuery = app.scrollViews.otherElements
         elementsQuery.buttons["[Demo] PXLGridView -> PXLPhotoProductView"].tap()
-        
-        //elementsQuery.collectionViews.children(matching: .cell).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.tap()
         
         let format = "label CONTAINS[c] %@"
 
