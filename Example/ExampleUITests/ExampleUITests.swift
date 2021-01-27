8a155dad62f1b6f90bf98939485f95fc5ef2f0bb
Sungjun <sungjun.app@gmail.com>
2021-01-27 15:35:18 +0900

    Fix a test

--- Example/ExampleUITests/ExampleUITests.swift
+++ Example/ExampleUITests/ExampleUITests.swift
@@ -37,12 +37,10 @@ class ExampleUITests: XCTestCase {
 
         let format = "label CONTAINS[c] %@"
 
-//        let label = app.staticTexts.element(matching: .any, identifier: PXLAnalyticsService.TAG)
-//        expectation(for: NSPredicate(format: format, "openedWidget"), evaluatedWith: label, handler: nil)
-//        waitForExpectations(timeout: 3, handler: nil)
-
+        let label = app.staticTexts.element(matching: .any, identifier: PXLAnalyticsService.TAG)
+        expectation(for: NSPredicate(format: format, "openedWidget"), evaluatedWith: label, handler: nil)
+        waitForExpectations(timeout: 3, handler: nil)
         
-        XCTAssertTrue(app.staticTexts.containing(NSPredicate(format: format, "openedWidget")).count>0)
         XCTAssertTrue(app.staticTexts.containing(NSPredicate(format: format, "widgetVisible")).count>0)
 
         app.collectionViews.children(matching: .cell).element(boundBy: 0).buttons["PXLPhotoProductView"].tap()
