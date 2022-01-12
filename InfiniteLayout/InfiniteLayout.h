7b52d5b02890a2375e24715fdacc6264f611cd1b
SungjunApp <sungjun.app@gmail.com>
2022-01-12 15:18:25 +0900

    Support Carthage

---
+++ InfiniteLayout/InfiniteLayout.h
@@ -0,0 +1,19 @@
+//
+//  InfiniteLayout.h
+//  InfiniteLayout
+//
+//  Created by Sungjun Hong on 1/12/22.
+//  Copyright © 2022 Pixlee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InfiniteLayout.
+FOUNDATION_EXPORT double InfiniteLayoutVersionNumber;
+
+//! Project version string for InfiniteLayout.
+FOUNDATION_EXPORT const unsigned char InfiniteLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InfiniteLayout/PublicHeader.h>
+
+
---
+++ PixleeSDK/PixleeSdk.h
@@ -0,0 +1,19 @@
+//
+//  PixleeSdk.h
+//  PixleeSdk
+//
+//  Created by Sungjun Hong on 1/11/22.
+//  Copyright © 2022 Pixlee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PixleeSdk.
+FOUNDATION_EXPORT double PixleeSdkVersionNumber;
+
+//! Project version string for PixleeSdk.
+FOUNDATION_EXPORT const unsigned char PixleeSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PixleeSdk/PublicHeader.h>
+
+
